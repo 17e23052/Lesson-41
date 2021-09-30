0010ad58477a9fce647e9a4b4d5a40c9d8477356
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-30 08:15:10 +0000

    A1 C2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-numbers = [0,1,1,2,3,5,8,13,21,34,55,89,134,223,357,580,937,1517,2454,3971]
+print("What times table do you want to see?")
+timestable = int(input())
+numbers = [str(1*timestable),str(2*timestable),str(3*timestable),str(4*timestable),str(5*timestable),str(6*timestable),str(7*timestable),str(8*timestable),str(9*timestable),str(10*timestable),str(11*timestable),str(12*timestable)]
 str_numbers = []
 for number in numbers:
   str_numbers.append(str(number))
 
-data = ",".join(str_numbers)
+data = ",".join(numbers)
 
-file = open("numbers.csv", "w")
+file = open("timestables.csv", "w")
 file.write(data)
 file.close()
